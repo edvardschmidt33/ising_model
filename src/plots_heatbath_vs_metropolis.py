1aa61310d26598aababb786d086c65b2643e4ef4
edvardschmidt33 <edvardschmidt3333@gmail.com>
2026-02-23 14:42:31 +0100

    added plots for heat bath

---
+++ src/plots_heatbath_vs_metropolis.py
@@ -0,0 +1,68 @@
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+from analytical_sol import file_ret
+import matplotlib.ticker as ticker
+
+def file_ret_alg(L, alg):
+    if alg == 'heat_bath':
+        filename = f'./data/results_heat_bath_L{L}.json'
+    elif alg == 'metropolis':
+        filename = f'./data/results_task1_L{L}.json'
+    else:
+        raise SyntaxError(f'No algorithm named: {alg}')
+    with open(filename, "r") as f:
+        data = json.load(f)
+    E = np.array(data["E"])
+    M = np.array(data["M"])
+    CV = np.array(data["CV"])
+    X = np.array(data["X"])
+
+    T_list = np.array(data["T_list"])
+    J_list = np.array(data["J_list"])
+
+
+    U = np.array(data["U"])
+    return E, M, CV, X, U, J_list, T_list
+
+
+if __name__ == '__main__':
+    #List of B:s tested: [0.01, 0.05, 0.1, 0.5, 1]
+    ### Specify L ###
+    L = 32
+    E_met, M_met, CV_met, X_met, _, J_list, T_list = file_ret_alg(L, 'metropolis')
+    E_hb, M_hb, CV_hb, X_hb, _, _, _ = file_ret_alg(L, 'heat_bath')
+
+    f = plt.figure(figsize=(16,10))
+
+    f.suptitle(f"Comparison of positive and negative J with L = {L}",
+           fontsize=24)
+
+    sp =  f.add_subplot(2, 2, 1 );
+    plt.scatter(T_list, E_met, s=10, color='IndianRed', label = 'Metropolis')
+    plt.scatter(T_list, E_hb, s=10, color='RoyalBlue', label = 'Heat Bath')
+    plt.legend(loc="best", framealpha=0.6)
+    plt.xlabel("Temperature (T)", fontsize=20);
+    plt.ylabel("Energy ", fontsize=20);         plt.axis('tight');
+
+    sp =  f.add_subplot(2, 2, 2);
+    plt.scatter(T_list, M_met, s=10, color='IndianRed')
+    plt.scatter(T_list, M_hb, s=10, color='RoyalBlue')
+    plt.xlabel("Temperature (T)", fontsize=20);
+    plt.ylabel("|Magnetization| ", fontsize=20);         plt.axis('tight');
+
+    sp =  f.add_subplot(2, 2, 3 );
+    plt.scatter(T_list, CV_met, s=10, color='IndianRed')
+    plt.scatter(T_list, CV_hb, s=10, color='RoyalBlue')
+    plt.xlabel("Temperature (T)", fontsize=20);
+    plt.ylabel("Specific Heat ", fontsize=20);         plt.axis('tight');
+
+    sp =  f.add_subplot(2, 2, 4 );
+    plt.scatter(T_list, X_met, s=10, color='IndianRed')
+    plt.scatter(T_list, X_hb, s=10, color='RoyalBlue')
+    plt.xlabel("Temperature (T)", fontsize=20);
+    plt.ylabel("Susceptibility ", fontsize=20);         plt.axis('tight');
+
+
+    plt.savefig(f'./figs/task2_{L}_negJ')
+    plt.show()   
